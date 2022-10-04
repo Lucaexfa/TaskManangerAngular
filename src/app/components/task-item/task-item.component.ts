8b144ddd3bcb3feaaeb22d4d56fabd27772418e8
Luca <lucaexequielfarias@gmail.com>
2022-10-04 18:04:17 -0300

    App Original

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
+import {Task} from '../../Task'
+import {TASKS} from '../../mocks-tasks'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { outputAst } from '@angular/compiler';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task: Task = TASKS[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter
+  faTimes=faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
+
+}
---
+++ src/app/mocks-tasks.ts
@@ -0,0 +1,39 @@
+import {Task} from "./Task"
+
+
+export const TASKS = [
+    {
+        id: 1,
+        text: "lorem ipsum dolor sit amet",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true,
+
+
+    },
+    {
+        id: 2,
+        text: "lorem ipsum dolor sit amet",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true,
+
+
+    },
+    {
+        id: 3,
+        text: "lorem ipsum dolor sit amet",
+        day: 'Agosto 5 a las 12:00',
+        reminder: false,
+
+
+    },
+    {
+        id: 4,
+        text: "lorem ipsum dolor sit amet",
+        day: 'Agosto 5 a las 12:00',
+        reminder: false,
+
+
+    },
+
+
+]
